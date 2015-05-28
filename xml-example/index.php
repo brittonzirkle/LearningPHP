b1f17b4cb51c95b4d6225475bd1d972ffaff578d
jasonlucas <jason.tennant@outlook.com>
2015-05-28 06:20:37 -0700

    adding xml example

---
+++ xml-example/index.php
@@ -0,0 +1,34 @@
+<?php
+
+libxml_use_internal_errors(true);
+$xml = simplexml_load_file("people.xml");
+
+if ($xml !== false)
+{
+    print_r($xml);
+
+    echo "<br>";
+    echo "<br>";
+
+    foreach ($xml->children() as $person)
+    {
+        echo "$person->name<br>";
+        echo "$person->age<br>";
+        echo "$person->gender<br>";
+        echo "{$person->address->street}<br>";
+        echo "{$person->address->city}<br>";
+        echo "{$person->address->state}<br>";
+        echo "{$person->address->zip}<br>";
+
+        echo "<br>";
+    }
+}
+else
+{
+    echo "error loading file<br>";
+
+    foreach(libxml_get_errors() as $error)
+    {
+        echo "$error->message";
+    } 
+}
