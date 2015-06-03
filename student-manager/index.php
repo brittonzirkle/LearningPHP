486e9004f244d82c99698ae0646a307bf51b27d8
jasonlucas <jason.tennant@outlook.com>
2015-06-03 06:19:03 -0700

    getting file structure ready

--- student-manager/index.php
+++ student-manager/index.php
@@ -1,3 +1,42 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Accounts</title>
+</head>
+<body>
 
-// See an article explaining this code at http://xcodula.com/student-management-app-in-php-part-1/
+<h2>Student Account List</h2>
+
+<table>
+    <thead>
+        <tr>
+            <th><a href="#">Id</a></th>
+            <th><a href="#">First Name</a></th>
+            <th><a href="#">Last Name</a></th>
+            <td></td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><a href="#">111</a></td>
+            <td>Albert</td>
+            <td>Einstein</td>
+            <td><a href="#">delete</a></td>
+        </tr>
+        <tr>
+            <td><a href="#">222</a></td>
+            <td>Robert</td>
+            <td>Oppenheimer</td>
+            <td><a href="#">delete</a></td>
+        </tr>
+        <tr>
+            <td><a href="#">333</a></td>
+            <td>Stephen</td>
+            <td>Hawking</td>
+            <td><a href="#">delete</a></td>
+        </tr>
+    </tbody>
+</table>
+
+</body>
+</html>
